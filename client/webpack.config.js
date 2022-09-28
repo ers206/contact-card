5c0469a09313042e223095f008985611ea314894
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-28 18:34:24 -0500

    starting 19.4.5 seems to be working fine

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+// this was in 19.4.4 it just said that the updated webpack file shwould have this/ i didnt have that 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = { 
     mode: "development",
@@ -12,7 +15,9 @@ module.exports = {
         new HtmlWebpackPlugin({
           template: './index.html',
           title: 'Webpack Plugin',
-        })
+        }),
+        // not sure if this is in the right place 
+        new WorkboxPlugin.GenerateSW()
       ],
     // line below not in 19.2.3 when it says to review code 
     // mode: "development", 
