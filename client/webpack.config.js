f6591af64e8b3ee97a5c1ec552f977204739bab2
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-29 14:33:04 -0500

    done with 19.4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,43 +1,46 @@
-const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+const path = require('path');
 
-module.exports = { 
-    mode: "development",
-    entry: "./src/js/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: './index.html',
-          title: 'Webpack Plugin',
-        })
-      ],
-    // line below not in 19.2.3 when it says to review code 
-    // mode: "development", 
-    module: {
-        rules: [
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              ]
-            }
-          }
-        }
-        ]
-      }
+        },
+      },
+    ],
+  },
 };
